6c5a4501144968be431ff62d08d2990917853d0f
Omolayo O <mollyqueen_o@yahoo.com>
2021-06-25 14:12:01 +0100

    updated Frontend pages June 25

--- ims.js
+++ ims.js
@@ -1,15 +1,16 @@
+$(function(){
 
 var sideBarIsOpen = true;
 
 toggleBtn.addEventListener( 'click', (event) => {
     event.preventDefault();
     if(sideBarIsOpen) {
-        sidebar_container.style.width = '10%';
-        sidebar_container.style.height = '100vh';
+        sidebar_container.style.width = '150px';
+        sidebar_container.style.height = '100%';
         sidebar_container.style.transition = '0.3s all';
         rightPageContent_container.style.width = '100%';
-        rightPageContent_container.style.marginLeft = '120px';
-        headtitle.style.fontSize = '10px';  
+        rightPageContent_container.style.marginLeft = '150px';
+        headtitle.style.fontSize = '15px';  
             
         menuText = document.getElementsByClassName('menuText');
         for(var i=0; i < menuText.length; i++){
@@ -17,9 +18,9 @@ toggleBtn.addEventListener( 'click', (event) => {
             sideBarIsOpen = false;
         } 
     } else { 
-        sidebar_container.style.width = '25%';
-        rightPageContent_container.style.width = '90%';
-        rightPageContent_container.style.marginLeft = '330px';
+        sidebar_container.style.width = '300px';
+        rightPageContent_container.style.width = '100%';
+        rightPageContent_container.style.marginLeft = '300px';
         headtitle.style.fontSize = '30px';   
             
         menuText = document.getElementsByClassName('menuText');
@@ -29,3 +30,86 @@ toggleBtn.addEventListener( 'click', (event) => {
         sideBarIsOpen = true;
         }
 });
+/*====================== Category ========================*/
+
+    $('#formCategory').submit(function(e){
+        console.log('function Test for Category')
+        // $.ajax({
+        //     type: 'POST',
+        //     url: '',
+        //     data: $('#formCategory').serialize(),
+        //     success: function(data){ 
+        //         console.log('data')
+        //     }
+        // })
+        e.preventDefault();
+    }) 
+
+
+/*====================== Inventory ========================*/
+
+    $('#formInventory').submit(function(e){
+        console.log('function Test Inventory')
+        // $.ajax({
+        //     type: 'POST',
+        //     url: '',
+        //     data: $('#formCategory').serialize(),
+        //     success: function(data){ 
+        //         console.log('data')
+        //     }
+        // })
+
+        e.preventDefault();
+    })
+    
+    
+/*====================== Purchase ========================*/
+
+    $('#formPurchase').submit(function(e){
+        console.log('function Test Purchase')
+        // $.ajax({
+        //     type: 'POST',
+        //     url: '',
+        //     data: $('#formCategory').serialize(),
+        //     success: function(data){ 
+        //         console.log('data')
+        //     }
+        // })
+        e.preventDefault();
+    }) 
+
+    
+/*====================== Sale ========================*/
+
+    $('#formSale').submit(function(e){
+        console.log('function Test Sale')
+        // $.ajax({
+        //     type: 'POST',
+        //     url: '',
+        //     data: $('#formCategory').serialize(),
+        //     success: function(data){ 
+        //         console.log('data')
+        //     }
+        // })
+        e.preventDefault();
+    })
+
+
+/*====================== Vendor ========================*/
+    
+    $('#formVendor').submit(function(e){
+        console.log('function Test Vendor')
+        // $.ajax({
+        //     type: 'POST',
+        //     url: '',
+        //     data: $('#formCategory').serialize(),
+        //     success: function(data){ 
+        //         console.log('data')
+        //     }
+        // })
+        e.preventDefault();
+    })
+
+
+    
+})
